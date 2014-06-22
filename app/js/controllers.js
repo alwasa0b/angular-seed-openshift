d287462ce47a253e72e81c31b43304c07d0afc6d
Saeed Alalwan <alwasa0b@users.noreply.github.com>
2014-06-21 23:03:41 -0400

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,10 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', [function(data) {
+    console.log(data)
 
   }])
-  .controller('MyCtrl2', [function() {
-
+  .controller('MyCtrl2', [function(data) {
+    console.log(data)
   }]);
